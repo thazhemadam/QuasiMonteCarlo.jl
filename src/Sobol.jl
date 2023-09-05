d03c89bd7932f4d962fa24673bc3b6c6538b4633
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2023-09-05 08:37:41 +0000

    allow generation of samples for non-powers of 2 for Sobol nets
    Sobol nets don't exist for n that isn't a power of 2, yet we allow it.
    Fix this up by disallowing this behaviour.

--- src/Sobol.jl
+++ src/Sobol.jl
@@ -8,7 +8,7 @@ Base.@kwdef @concrete struct SobolSample <: DeterministicSamplingAlgorithm
 end
 
 function sample(n::Integer, d::Integer, S::SobolSample, T = Float64)
+    @assert isinteger(log(2, n)) "Sobol nets exist only for sample sizes that are powers of 2."
     s = Sobol.SobolSeq(zeros(T, d), ones(T, d))
-    skip(s, n)
     return randomize(reduce(hcat, [next!(s) for i in 1:n]), S.R)
 end
